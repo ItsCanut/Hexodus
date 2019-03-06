f18ed22d5a7771a72538f2fb36f44ec311050561
Carlos_Canut <ItsCanut@users.noreply.github.com>
2019-03-06 12:18:45 +0100

    Camara (en proceso, falta que el arma siga el movimiento)
    La camara funciona a falta de hacer que el arma siga el movimiento de la camara.

--- Hexodus/Assets/Player/Camera_Control.cs
+++ Hexodus/Assets/Player/Camera_Control.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class Camera_Control : MonoBehaviour
 {
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
 
-    public Camera FPSCamera;
+    [SerializeField] private Transform playerBody;
 
-    public float horizontalSpeed;
-    public float verticalSpeed;
+    private float GanchoAxisx;
 
-    float h;
-    float v;
-
-    void Start()
+    private void Awake()
     {
-        
+        LockCursor();
+        GanchoAxisx = 0.0f;
     }
 
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void Update()
     {
-        h = horizontalSpeed * Input.GetAxis("Mouse X");
-        v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-        transform.Rotate(0, h, 0);
-        FPSCamera.transform.Rotate(-v, 0, 0);
-
-        
+        CameraRotation();
     }
 
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
+        GanchoAxisx += mouseY;
+
+        if (GanchoAxisx > 90.0f)
+        {
+            GanchoAxisx = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (GanchoAxisx < -90.0f)
+        {
+            GanchoAxisx = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
 }

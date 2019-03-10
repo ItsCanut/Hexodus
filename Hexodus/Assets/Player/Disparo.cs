692df3f876c8698c358d2c48c5312ae405732031
Jocarfl <jcarreresjr@hotmail.com>
2019-03-10 23:42:08 +0100

    Sonido,efectos laser y nueva arma laser

--- Hexodus/Assets/Player/Disparo.cs
+++ Hexodus/Assets/Player/Disparo.cs
@@ -7,14 +7,31 @@ public class Disparo : MonoBehaviour
     // Start is called before the first frame update
       public Rigidbody projectile;
     public float speed = 20;
- 
+    public AudioClip AudioDisparo ;
+    public GameObject efecto;
+
+    private void Start()
+    {
+        efecto.active = false;
+    }
     // Update is called once per frame
     void Update ()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            
             Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation)as Rigidbody;
             instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0,speed));
+            transform.GetComponent<AudioSource>().PlayOneShot(AudioDisparo);
+
+               efecto.active = true;
+            Invoke("FX", 0.2f);
         }
+    }
+
+
+    void FX()
+    {
+        efecto.active=false;
     }
 }

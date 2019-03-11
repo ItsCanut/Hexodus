253bd518b8f7dabea3580441251a421953b7d542
Jocarfl <jcarreresjr@hotmail.com>
2019-03-11 03:50:25 +0100

    Enemigos con vida son eliminados

--- Hexodus/Assets/Player/Disparo.cs
+++ Hexodus/Assets/Player/Disparo.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.CinemachineTargetGroup;
 
 public class Disparo : MonoBehaviour
 {
     // Start is called before the first frame update
       public Rigidbody projectile;
-    public float speed = 20;
+    public float speed = 20f;
+    public float Dano = 10f;
+    public float range = 500f;
     public AudioClip AudioDisparo ;
     public GameObject efecto;
 
+    public Camera fpsCam;
     private void Start()
     {
         efecto.active = false;
@@ -19,13 +23,10 @@ public class Disparo : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            
-            Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation)as Rigidbody;
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0,speed));
-            transform.GetComponent<AudioSource>().PlayOneShot(AudioDisparo);
 
-               efecto.active = true;
-            Invoke("FX", 0.2f);
+
+            Shoot();
+            
         }
     }
 
@@ -33,5 +34,25 @@ public class Disparo : MonoBehaviour
     void FX()
     {
         efecto.active=false;
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        //Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+        //instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        transform.GetComponent<AudioSource>().PlayOneShot(AudioDisparo);
+        efecto.active = true;
+        Invoke("FX", 0.2f);
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range))
+        {
+            Debug.Log(hit.transform.name);
+            VidaIA target = hit.transform.GetComponent<VidaIA>();
+            if(target != null)
+            {
+                
+                target.AplicarDaño(Dano);
+            }
+        }
+
     }
 }

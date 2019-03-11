6e128903f284aa517414aa0e217d47a2bb9a8233
EMILIO <emilioesteve99@icloud.com>
2019-03-11 23:59:40 +0100

    HUD
    Hecho el HUD e implemenatado el cronometro

---
+++ Hexodus/Assets/Seconds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Seconds : MonoBehaviour
+{
+    
+    public DateTime tiempo1;
+    public GameObject segundos;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo1 = DateTime.Now;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime tiempo2 = DateTime.Now;
+        
+        TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+        Text text = segundos.GetComponent<Text>();
+        int loSegundos = total.Seconds;
+        if (loSegundos >= 60)
+        {
+            loSegundos -= 60;
+        }
+
+        text.text = ":" + loSegundos.ToString();
+
+    }
+}

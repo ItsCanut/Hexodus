1a2c6cccc0d762eadb6b503259f447d70941fdb2
Josep Carreres <jcarreresjr@hotmail.com>
2019-03-12 11:29:13 +0100

    Demo prueba(respanws y disparos)

---
+++ Hexodus/Assets/VidaP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaP : MonoBehaviour
+{
+    int vida = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  public void AplicarDano(int dano)
+    {
+        vida -= dano;
+
+        if (vida <=0)
+        {
+            Destroy(gameObject);
+        }
+
+    }  
+
+}

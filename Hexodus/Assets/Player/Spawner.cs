5df12075e8cd660cd2baa6c265689b70af19f8ea
emilioesteve <36475580+emilioesteve@users.noreply.github.com>
2019-03-24 23:53:05 +0100

    HUD Y SPAWNS
    Acabadas algunas de las funcionalidades del hud. Organizado para escalabilidad y comentado el código de spawns para futuras intervenciones.

--- Hexodus/Assets/Player/Spawner.cs
+++ Hexodus/Assets/Player/Spawner.cs
@@ -9,24 +9,24 @@ public class Spawner : MonoBehaviour
 {
 
     //GameObjects  
-    public GameObject Enemigo;
+    public GameObject robot;
     public int cuantosQuieroSpawnear;
     public GameObject nRonda;
     public GameObject nRestantes;
     int ronda;
-    
+    Text text;
+    bool hayEnemigos = true;
+
 
 
     void Start()
     {
-        //Enemigos que spawnean cuando iniciamos el juego.
-        for( int i = 0; i < cuantosQuieroSpawnear; i++)
-        {
-            spawnEnemy();
-        }
 
-        cuantosQuieroSpawnear++;
-        Text text = nRonda.GetComponent<Text>();
+        //Enemigos que spawnean cuando iniciamos el juego.
+        spawnEnemy(robot, cuantosQuieroSpawnear);
+        
+        cuantosQuieroSpawnear++; // cada ronda spawnea un enemigo mas
+        text = nRonda.GetComponent<Text>();
         ronda = Convert.ToInt16(nRonda) + 1;
         text.text = ronda.ToString();
     }
@@ -42,30 +42,38 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
-        int enemigosRestantes = GameObject.FindGameObjectsWithTag("Robot").Length;
-        if ( enemigosRestantes == 1)
+        int enemigosRestantes = GameObject.FindGameObjectsWithTag("Robot").Length; // devuelve el numero de robots que hay en el mapa
+        if ( enemigosRestantes == 1) // siempre habrá uno, ya que es el que clonaremos en la función de spawn.
+            //Si hay más de uno significa que no has acabado con la oleada y por lo tanto no entrará en el if
         {
-            for(int i = 0; i < cuantosQuieroSpawnear; i++)
-            {
-                spawnEnemy();
-            }
-            Text text = nRonda.GetComponent<Text>();
+            spawnEnemy(robot, cuantosQuieroSpawnear);
+            // cuando solo quede un enemigo será cuando spawneen más y cuando se sumará una ronda más al marcador.           
+            cuantosQuieroSpawnear++;    // cada ronda spawneará un enemigo más.     
+        }
+
+
+        if( hayEnemigos.Equals(false))
+        {
+            text = nRonda.GetComponent<Text>();
             ronda = Convert.ToInt16(nRonda) + 1;
             text.text = ronda.ToString();
-            
-            cuantosQuieroSpawnear++;
-            cuantosQuieroSpawnear++;           
+            hayEnemigos = false;
         }
 
         Text restantes = nRestantes.GetComponent<Text>();
 
-        restantes.text = enemigosRestantes.ToString();
+        //Para ponerlo en el marcador no contaremos a ese enemigo que clonamos.
+        restantes.text = (enemigosRestantes - 1).ToString();
 
     }
-    // spawns an enemy based on the enemy level that you selected
-    private void spawnEnemy()
+
+    // con esta función lo que hacemos es clonar el gameobject que queramos y las veces que queramos. 
+    private void spawnEnemy(GameObject elEnemigo, int cuantos)
     {
-        Instantiate(Enemigo, transform.position, Quaternion.identity);
+        for( int i = 0; i < cuantos; i++)
+        {
+            Instantiate(elEnemigo, transform.position, Quaternion.identity);
+        }
     }
 
 }

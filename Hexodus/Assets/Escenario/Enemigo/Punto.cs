388a086ef63d8bec08949719e4d2d5a707d743a6
Josep Carreres <jcarreresjr@hotmail.com>
2019-04-16 12:24:06 +0200

    Balas y VidaCastilla funcionando

--- Hexodus/Assets/Escenario/Enemigo/Punto.cs
+++ Hexodus/Assets/Escenario/Enemigo/Punto.cs
@@ -8,12 +8,50 @@ public class Punto : MonoBehaviour
 
     public float VidaTorre = 1000f;
     public Scrollbar HealthBar;
-    // Start is called before the first frame update
+    public GameObject VidaCast;
+    Image image;
+    private int cont = 0;
+    public Sprite cas1;
+    public Sprite cas2;
+    public Sprite cas3;
+    public Sprite cas4;
 
+    void Update() 
+    {
+        image = VidaCast.GetComponent<Image>();
+    }
     public void AplicarDañoTorre(float amount)
     {
-        VidaTorre -= amount;
        
+        
+        VidaTorre -= amount;
+        cont++;
+
+        if (cont==1)
+        {
+
+            Debug.Log("Hola1");
+            image.sprite = cas1;
+            
+        }
+        if (cont == 2)
+        {
+            Debug.Log("Hola2");
+            image.sprite = cas2;
+            
+        }
+        if (cont == 3)
+        {
+            Debug.Log("Hola3");
+            image.sprite = cas3;
+           
+        }
+        if (cont == 4)
+        {
+            image.sprite = cas4;
+            Debug.Log("Hola4");
+        }
+
         if (VidaTorre <= 0f)
         {
            
